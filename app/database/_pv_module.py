f815753c3e353b7465459b27776c50063ca376e2
agittarius <tobias.schmocker@gmail.com>
2019-04-02 12:25:40 +0200

    - db-table for pv-modules implemented - html-table for pv-modules implemented

--- app/database/_pv_module.py
+++ app/database/_pv_module.py
@@ -4,35 +4,50 @@ from ..app import db
 class PvModule(db.Model):
     __tablename__ = __qualname__
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, info={'label': '#'})
 
-    model = db.Column(db.String(80), nullable=False)
-    manufacturer = db.Column(db.String(120), nullable=False)
-    cell_type = db.Column(db.String(80), nullable=False)
-    additional_information = db.Column(db.String(80), nullable=False)
-    price_CHF = db.Column(db.Float, nullable=False)
-    length = db.Column(db.Float, nullable=False) # [m]
-    width = db.Column(db.Float, nullable=False) # [m]
-    shunt_resistance_Ohm = db.Column(db.Float, nullable=False) # [Ohm]
+    model = db.Column(db.String(80), nullable=False, info={'label': 'Modell'})
+    manufacturer = db.Column(db.String(120), nullable=False, info={'label': 'Hersteller'})
+    cell_type = db.Column(db.String(80), nullable=False, info={'label': 'Zell-Typ'})
+    additional_information = db.Column(db.String(80), nullable=False, info={'label': 'Infos'})
+    price_CHF = db.Column(db.Float, nullable=False, info={'label': 'Preis', 'unit': 'CHF'})
+    length = db.Column(db.Float, nullable=False, info={'label': 'Länge', 'unit': 'm'})
+    width = db.Column(db.Float, nullable=False, info={'label': 'Breite', 'unit': 'm'})
+    shunt_resistance_Ohm = db.Column(db.Float, nullable=False, info={'label': 'Shunt-Widerstand', 'unit': 'Ohm'})
 
     # manufacturer data
-    manu_mpp_voltage = db.Column(db.Float, nullable=False) # [V]
-    manu_mpp_current = db.Column(db.Float, nullable=False) # [A]
-    manu_oc_voltage = db.Column(db.Float, nullable=False) # [V]
-    manu_sc_current = db.Column(db.Float, nullable=False) # [A]
-    manu_form_factor = db.Column(db.Float, nullable=False) # [-]
-    manu_voltage_temperature_coef_UL = db.Column(db.Float, nullable=False) # [ % /°C]
-    manu_current_temperature_coef_UL = db.Column(db.Float, nullable=False) # [ % /°C]
+    manu_mpp_voltage = db.Column(db.Float, nullable=False,
+                                 info={'label': 'Spannung bei MPP', 'unit': 'V', 'origin': 'Hersteller'})
+    manu_mpp_current = db.Column(db.Float, nullable=False,
+                                 info={'label': 'Strom bei MPP', 'unit': 'A', 'origin': 'Hersteller'})
+    manu_oc_voltage = db.Column(db.Float, nullable=False,
+                                info={'label': 'Leerlaufspannung', 'unit': 'V', 'origin': 'Hersteller'})
+    manu_sc_current = db.Column(db.Float, nullable=False,
+                                info={'label': 'Kurzschlussstrom', 'unit': 'A', 'origin': 'Hersteller'})
+    manu_form_factor = db.Column(db.Float, nullable=False,
+                                 info={'label': 'Form-Faktor', 'unit': '-', 'origin': 'Hersteller'})
+    manu_voltage_temperature_coef_UL = db.Column(db.Float, nullable=False,
+                                                 info={'label':  'Spannungs-Temperatur-Koeffizient', 'unit': '% / °C',
+                                                       'origin': 'Hersteller'})
+    manu_current_temperature_coef_UL = db.Column(db.Float, nullable=False,
+                                                 info={'label':  'Strom-Temperatur-Koeffizient', 'unit': '% / °C',
+                                                       'origin': 'Hersteller'})
 
     # flasher data
-    flasher_radiation = db.Column(db.Float, nullable=False) # [W / m2]
-    flasher_module_temperature = db.Column(db.Float, nullable=False) # [°C]
-
-    flasher_mpp_voltage = db.Column(db.Float, nullable=False) # [V]
-    flasher_mpp_current = db.Column(db.Float, nullable=False) # [A]
-    flasher_oc_voltage = db.Column(db.Float, nullable=False) # [V]
-    flasher_sc_current = db.Column(db.Float, nullable=False) # [A]
-    flasher_form_factor = db.Column(db.Float, nullable=False) # [-]
+    flasher_radiation = db.Column(db.Float, nullable=False,
+                                  info={'label': 'Einstrahlung', 'unit': 'W / m2', 'origin': 'Flasher-Messung'})
+    flasher_module_temperature = db.Column(db.Float, nullable=False, info={'label':  'Modul-Temperatur',
+                                                                           'unit': '°C', 'origin': 'Flasher-Messung'})
+    flasher_mpp_voltage = db.Column(db.Float, nullable=False,
+                                    info={'label': 'Spannung bei MPP', 'unit': 'V', 'origin': 'Flasher-Messung'})
+    flasher_mpp_current = db.Column(db.Float, nullable=False,
+                                    info={'label': 'Strom bei MPP', 'unit': 'A', 'origin': 'Flasher-Messung'})
+    flasher_oc_voltage = db.Column(db.Float, nullable=False,
+                                   info={'label': 'Leerlaufspannung', 'unit': 'V', 'origin': 'Flasher-Messung'})
+    flasher_sc_current = db.Column(db.Float, nullable=False,
+                                   info={'label': 'Kurzschlussstrom', 'unit': 'A', 'origin': 'Flasher-Messung'})
+    flasher_form_factor = db.Column(db.Float, nullable=False,
+                                    info={'label': 'Form-Faktor', 'unit': '-', 'origin': 'Flasher-Messung'})
 
     def __repr__(self):
         return f'PV-Modul ({self.model})'
--- app/routes/_pv_modules.py
+++ app/routes/_pv_modules.py
@@ -1,12 +1,46 @@
-from flask import Blueprint, render_template, abort, request
+from flask import Blueprint, render_template, abort, request, redirect, flash
 import numpy as np
 import json
 
-from ..database import _pv_module
+from ..database import PvModule
 
 pv_modules_routes = Blueprint('pv', __name__, template_folder='templates')
 
 
 @pv_modules_routes.route('/pv_modules')
 def pv_modules():
-    return render_template('pv/pv_modules.html')
+    modules = PvModule.query.all()
+    return render_template('pv/pv_modules.html', modules=modules)
+
+
+@pv_modules_routes.route('/pv_module')
+def pv_module():
+    try:
+        id = request.args.get('id', type=int)
+        if id is None:
+            raise Exception(f'no valid id for pv module')
+        module = PvModule.query.get(id)
+        if module is None:
+            raise Exception(f'no pv module with id {id} exists')
+        return render_template('pv/pv_module.html', module=module)
+    except Exception as e:
+        flash(str(e), category='danger')
+        return redirect('pv_modules')
+
+
+@pv_modules_routes.route('/pv_modules/add')
+def add_pv_module():
+    pass
+    # todo: add-form
+
+
+@pv_modules_routes.route('/pv_modules/edit')
+def edit_pv_module():
+    id = request.args.get('id', type=int)
+    # todo: edit-form (evt. combine edit and add form if possible)
+
+
+@pv_modules_routes.route('/pv_modules/remove')
+def remove_pv_module():
+    id = request.args.get('id', type=int)
+    # todo: remove request

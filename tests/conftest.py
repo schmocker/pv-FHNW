09884c00ce9c4997b5099530f461b5b3240dfbbe
gigantenbein <ma.gantenbein@gmail.com>
2019-04-09 14:40:58 +0200

    added client and runner fixture to conftest and renamed test_factory to conftest

---
+++ tests/conftest.py
@@ -0,0 +1,38 @@
+import os
+import tempfile
+
+import pytest
+
+from app import create_app
+
+
+@pytest.fixture
+def client():
+    """skeleton for testing that is coming
+    db_fd: file handle for temporary database"""
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path
+    })
+
+    with app.app_context():
+        app.init_db()
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    """ Use client to make requests to application"""
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    """ Create runner that registers click commands"""
+    return app.test_cli_runner()
